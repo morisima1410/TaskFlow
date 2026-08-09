84c272b19c89ea76ae6955668668edb575e7f838
Mori Sima <morisima1410@gmail.com>
2026-08-09 23:36:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -432,6 +432,6 @@ app.get('*', (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`TASKFLOW Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`TASKFLOW Server running on http://0.0.0.0:${PORT}`);
 });
